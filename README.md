b5d3e041abe83c8765fcdf3ad2fcab3ddf447e3d
Andre Tadeu de Carvalho <andre.tadeu.de.carvalho@gmail.com>
2016-04-29 22:29:09 -0300

    Glassfish cluster without load balancer.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Glassfish
+
+Glassfish Docker image based on https://github.com/oracle/docker-images/tree/master/GlassFish/4.1.1
+
+## How to user
+
+First, you have to create a Domain Administration Server, or DAS for short.
+
+```bash
+$ docker run -d --name my_das -p 3700:3700 -p 4848:4848 -p 7676:7676 -p 8080:8080 \
+-p 8181:8181 -p 8686:8686 -e 'DAS=true' andretadeu/glassfish:latest
+```
+
+After that, you create any number of nodes you want. Below I created two nodes, so:
+
+```bash
+$ docker run -d --name node-1 --link my_das:das -p 3701:3700 -p 4849:4848 \
+-p 7677:7676 -p 8081:8080 -p 8182:8181 -p 8687:8686 andretadeu/glassfish:latest
+$ docker run -d --name node-2 --link my_das:das -p 3702:3700 -p 4850:4848 \
+-p 7678:7676 -p 8082:8080 -p 8183:8181 -p 8688:8686 andretadeu/glassfish:latest
+```
--- run.sh
+++ run.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
+service ssh start
 asadmin start-domain
 # Create Cluster
 if [ -n "${DAS}" ]
 then
   asadmin --user=admin --passwordfile=/tmp/glassfishpwd --interactive=false create-cluster cluster1
+  asadmin --user=admin stop-domain
+  asadmin start-domain -v
 fi
-asadmin --user=admin stop-domain
+if [ -n "${DAS_PORT_4848_TCP_ADDR}" ]
+then
+  # Create cluster node
+  asadmin --user=admin --passwordfile=/tmp/glassfishpwd --interactive=false \
+--host das --port 4848 create-local-instance --cluster cluster1 cluster1-"${HOSTNAME}"
 
-service ssh start
-asadmin start-domain -v
+  # Stop domain
+  asadmin --user=admin stop-domain
+
+  ssh-keyscan -H das >> /root/.ssh/known_hosts
+
+  DAS_STATUS=$(ssh root@das /glassfish4/glassfish/bin/asadmin --user=admin \
+--passwordfile=/tmp/glassfishpwd list-domains | head -n 1)
+
+  while [ "${DAS_STATUS}" = "domain1 not running" ]
+  do
+    sleep 20
+    DAS_STATUS=$(ssh root@das /glassfish4/glassfish/bin/asadmin --user=admin \
+  --passwordfile=/tmp/glassfishpwd list-domains | head -n 1)
+  done
+
+  # Update existing CONFIG node to a SSH one
+  ssh root@das /glassfish4/glassfish/bin/asadmin --user=admin \
+--passwordfile=/tmp/glassfishpwd --interactive=false update-node-ssh \
+--sshuser root --sshkeyfile /root/.ssh/id_rsa \
+--nodehost $(cat /etc/hosts | grep "${HOSTNAME}" | cut -f1 -s) \
+--installdir /glassfish4 "${HOSTNAME}"
+
+  while [[ true ]]; do
+    sleep 1
+  done
+fi
