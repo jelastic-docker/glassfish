8623a7707378be98a3ca06ed9ed80296345d1bf3
Ruslan Synytsky <rs@jelastic.com>
2016-11-20 13:30:27 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 start() {
-    service ssh start
     /glassfish4/bin/asadmin start-domain
 
     # Create Cluster
@@ -21,26 +20,26 @@ start() {
         /glassfish4/bin/asadmin --user=admin stop-domain
 
         # Getting all keys from Domain Administration Server SSH
-        ssh-keyscan -H das >> /root/.ssh/known_hosts
+        ssh-keyscan -H das >> /"${USER}"/.ssh/known_hosts
 
         # Busy waiting for SSH to be enabled
-        SSH_STATUS=$(ssh root@das echo "I am waiting.")
+        SSH_STATUS=$(ssh ${USER}@das echo "I am waiting.")
         echo $SSH_STATUS >> /var/log/run.log
         while [ "${SSH_STATUS}" = "ssh: connect to host das port 22: Connection refused" || "${SSH_STATUS}" = "Host key verification failed."]
         do
             sleep 20
-            SSH_STATUS=$(ssh root@das echo "I am waiting.")
+            SSH_STATUS=$(ssh ${USER}@das echo "I am waiting.")
             echo $SSH_STATUS >> /var/log/run.log
         done
 
         # Busy waiting for Domain Administration Server to be available
-        DAS_STATUS=$(ssh root@das /glassfish4/glassfish/bin/asadmin --user=admin \
+        DAS_STATUS=$(ssh ${USER}@das /glassfish4/glassfish/bin/asadmin --user=admin \
         --passwordfile=/opt/glassfishpwd list-domains | head -n 1)
 
         while [ "${DAS_STATUS}" = "domain1 not running" ]
         do
             sleep 20
-            DAS_STATUS=$(ssh root@das /glassfish4/glassfish/bin/asadmin --user=admin \
+            DAS_STATUS=$(ssh ${USER}@das /glassfish4/glassfish/bin/asadmin --user=admin \
             --passwordfile=/opt/glassfishpwd list-domains | head -n 1)
         done
 
@@ -52,13 +51,13 @@ start() {
         fi
 
         # Update existing CONFIG node to a SSH one
-        ssh root@das /glassfish4/glassfish/bin/asadmin --user=admin \
+        ssh ${USER}@das /glassfish4/glassfish/bin/asadmin --user=admin \
         --passwordfile=/opt/glassfishpwd --interactive=false update-node-ssh \
-        --sshuser root --sshkeyfile /root/.ssh/id_rsa \
+        --sshuser "${USER}" --sshkeyfile /"${USER}"/.ssh/id_rsa \
         --nodehost "${HOST_IP}" --installdir /glassfish4 "${HOSTNAME}"
 
         # Start instance
-        ssh root@das /glassfish4/glassfish/lib/nadmin --user=admin \
+        ssh ${USER}@das /glassfish4/glassfish/lib/nadmin --user=admin \
         --passwordfile=/opt/glassfishpwd --interactive=false start-instance cluster1-"${HOSTNAME}"
 
         while [[ true ]]; do
@@ -68,13 +67,13 @@ start() {
 }
 
 stop() {
-    ssh root@das /glassfish4/glassfish/lib/nadmin --user=admin \
+    ssh ${USER}@das /glassfish4/glassfish/lib/nadmin --user=admin \
     --passwordfile=/opt/glassfishpwd --interactive=false stop-instance cluster1-"${HOSTNAME}"
 
-    ssh root@das /glassfish4/glassfish/lib/nadmin --user=admin \
+    ssh ${USER}@das /glassfish4/glassfish/lib/nadmin --user=admin \
     --passwordfile=/opt/glassfishpwd --interactive=false delete-instance cluster1-"${HOSTNAME}"
 
-    ssh root@das /glassfish4/glassfish/bin/asadmin --user=admin \
+    ssh ${USER}@das /glassfish4/glassfish/bin/asadmin --user=admin \
     --passwordfile=/opt/glassfishpwd --interactive=false delete-node-ssh "${HOSTNAME}"
 
     /glassfish4/glassfish/lib/nadmin --user=admin --passwordfile=/opt/glassfishpwd \
