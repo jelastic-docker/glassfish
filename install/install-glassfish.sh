a0ab2dfaae80c8fc2c8ecbcfb600260a07461a6c
Ruslan Synytsky <rs@jelastic.com>
2016-11-20 16:16:31 +0000

    Add new file

---
+++ install/install-glassfish.sh
@@ -0,0 +1,20 @@
+
+# Install Glassfish Application Server
+wget --quiet --no-check-certificate $GLASSFISH_URL
+echo "$MD5 *$GLASSFISH_PKG" | md5sum -c -
+unzip -o $GLASSFISH_PKG
+rm -f $GLASSFISH_PKG
+apt-get purge -yqq wget unzip && rm -rf /var/cache/apt/*
+
+echo "AS_ADMIN_PASSWORD=" > ~/glassfishpwd
+
+# Change Glassfish default user password
+echo "AS_ADMIN_NEWPASSWORD=${PASSWORD}" >> ~/glassfishpwd
+~/glassfish4/bin/asadmin --user=admin --passwordfile=~/glassfishpwd change-admin-password --domain_name domain1
+
+# Enable secure admin
+~/glassfish4/bin/asadmin start-domain
+echo "AS_ADMIN_PASSWORD=${PASSWORD}" > ~/glassfishpwd
+~/glassfish4/bin/asadmin --user=admin --passwordfile=~/glassfishpwd enable-secure-admin
+
+~/glassfish4/bin/asadmin --user=admin stop-domain
