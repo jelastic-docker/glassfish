7736aaf9cbdbf2bc4ea2176439fee5d72dea5543
sych74 <sych74@gmail.com>
2017-03-26 22:13:21 +0200

    Update install-root.sh

--- install/install-root.sh
+++ install/install-root.sh
@@ -3,4 +3,4 @@
 # Install OpenJDK 8 and some packages to removed later
 echo 'deb http://httpredir.debian.org/debian jessie-backports main' > /etc/apt/sources.list.d/jessie-backports.list
 apt-get -qq update
-apt-get install -yqq wget unzip openjdk-8-jdk openssh-server
+apt-get install -y wget unzip openjdk-8-jdk openssh-server
