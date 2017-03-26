225d47938285a617421df9370c16a20f0d0ec49d
sych74 <sych74@gmail.com>
2017-03-27 00:04:50 +0200

    Update glassfish.sh

--- glassfish.sh
+++ glassfish.sh
@@ -8,6 +8,9 @@ start() {
     	echo -e 'y\n'|ssh-keygen -t rsa -b 4096 -q -N '' -f ~/.ssh/id_rsa
     	cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
 
+        #update JMX default host
+        ~/glassfish4/bin/asadmin set --user=admin --passwordfile=${PSWD_FILE} server-config.jms-service.jms-host.default_JMS_host.host="${HOSTNAME}"
+        
         #start domain
         ~/glassfish4/bin/asadmin start-domain
         
